f0bfcfa608e55bf23b876707d7e55bacc3266a8c
Tom Butler <tom@r.je>
2017-05-01 19:40:25 +0100

    added comment

--- module/Library.php
+++ module/Library.php
@@ -1,29 +1,33 @@
 <?php
+
 namespace Axel\Module;
+
 class Library implements \Autoload\Module {
 	private $libraryDir;
 	private $axel;
+	private $rootNs;
 	private $cwd;
-		
-	public function __construct(\Axel\Axel $axel, $libraryDir) {
+
+	public function __construct(\Axel\Axel $axel, $libraryDir, $rootNs = null, $baseDir = null) {
 		$this->axel = $axel;
-		$this->libraryDir = getcwd() . DIRECTORY_SEPARATOR . str_replace('./', '', $libraryDir);
+		$this->cwd = is_null($baseDir) ? getcwd() : $baseDir;
+		$this->libraryDir = $this->cwd . DIRECTORY_SEPARATOR . ltrim(str_replace('./', '', $libraryDir), '/\\');
+		$this->rootNs = $rootNs;
 	}
-	
+
 	public function locate($className) {
 
-		$rootNs = strtolower(explode('\\', rtrim($className, '\\'))[0]);
+		$rootNs = is_null($this->rootNs) ? explode('\\', rtrim($className, '\\'))[0] . DIRECTORY_SEPARATOR : $this->rootNs;
 
+		if (file_exists($this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . 'autoload.json')) {
 
-		if (file_exists($this->libraryDir . DIRECTORY_SEPARATOR . strtolower($rootNs) . DIRECTORY_SEPARATOR . 'autoload.json')) {
+			$json = json_decode(file_get_contents($this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . 'autoload.json'));
 
-			$json = json_decode(file_get_contents($this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . DIRECTORY_SEPARATOR . 'autoload.json'));
-	
-			if (isset($json->include)) 	foreach ($json->include as $file) require_once $this->libaryDir . DIRECTORY_SEPARATOR . $rootNs . DIRECTORY_SEPARATOR . $file;
-	
+			if (isset($json->include)) 	foreach ($json->include as $file) require_once $this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . $file;
+
 			foreach ($json->modules as $key => $value) 	{
 				if ($key == 'Axel\\Module\\PSR0') {
-					$value[0] = $this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . DIRECTORY_SEPARATOR . $value[0];
+					$value[0] = $this->libraryDir . DIRECTORY_SEPARATOR . $rootNs . $value[0];
 				}
 				$module = new $key(...$value);
 				$this->axel->addModule($module);
--- module/PSR4.php
+++ module/PSR4.php
@@ -1,28 +1,45 @@
 <?php 
 namespace Axel\Module;
-class PSR0 implements \Autoload\Module {
+/*
+* PSR-4 compatible case insensitive autoloader
+* 
+* Goal to be 100% PSR-4 compatible but also case insensitive as any sane standard should have been
+* (unfortunately composer's lack of caching of PSR-4 loaded files makes it impractical so
+* it looks like case sensitivity was designed into PSR-4 to work around composer limitations)
+* 
+* See: https://r.je/php-autoloaders-should-not-be-case-sensitive.html
+* for an explanation of why autoloaders should be case insensitive
+*
+* Because all paths returned by axel modules are cached, the locate function is only run when
+* directory structures are added or a new class is added to the project
+* It doesn't matter if this is slow, it's only used on the first request. In production all
+* paths are cached and this class is never even loaded.
+*/
+class PSR4 implements \Autoload\Module {
 	private $baseDir;
 	private $namespace;
 	
 	public function __construct($baseDir, $namespace = null) {
 		if ($baseDir[0] == '/' || $baseDir[1] == ':') $this->baseDir = $baseDir; 
 		else $this->baseDir = getcwd() . DIRECTORY_SEPARATOR . str_replace(['./'], '', $baseDir);
-
+		$this->baseDir = rtrim($this->baseDir, DIRECTORY_SEPARATOR);
 		$this->namespace = trim($namespace, '\\');
 	}
 	
 	public function locate($className) {
 		if ($this->namespace != null) {
 			if (strpos(strtolower($className), strtolower($this->namespace)) === 0) {
-				$className = substr_replace($className, '', 0, strlen($this->namespace));
-			}
+				$className = ltrim(str_replace($this->namespace, '', $className), '\\');	
+			}	
 		}
 		
 		$parts = explode('\\', $className);
 
-		$file = $this->baseDir . DIRECTORY_SEPARATOR .
-			implode(DIRECTORY_SEPARATOR, $parts) .  '.php';
 
+		$file = $this->baseDir . DIRECTORY_SEPARATOR .
+				implode(DIRECTORY_SEPARATOR, $parts) .  '.php';
+
+		
 		//First check using case-sensitivity
 		if ($file !== null && is_file($file)) return $file;	 
 
@@ -31,9 +48,6 @@ class PSR0 implements \Autoload\Module {
 			$parts[$i] = strtolower($parts[$i]);
 			$file = $this->baseDir . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts) .  '.php';
 			if ($file !== null && is_file($file)) return $file;	 
-		}
-		
-		
-	}
-	
+		}		
+	}	
 }
