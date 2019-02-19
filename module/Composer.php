c652765916d1b0573a700c1b53640b375064dbcf
Tom Butler <tom@r.je>
2019-02-19 17:16:35 +0000

    better composer.json support

--- module/Composer.php
+++ module/Composer.php
@@ -1,61 +1,59 @@
 <?php
 namespace Axel\Module;
 class Composer implements \Autoload\Module {
-	private $libraryDir;
 	private $axel;
 	private $cwd;
-	private $file;
+	private $rootDir;
+	private $fileName = 'composer.json';
+	private $configured;
 
-	public function __construct(\Axel\Axel $axel, $libraryDir, $file = 'composer.json') {
+	public function __construct(\Axel\Axel $axel, $rootDir = '', $fileName = 'composer.json') {
 		$this->axel = $axel;
-		$this->file = $file;
-		$this->libraryDir = getcwd() . DIRECTORY_SEPARATOR . str_replace('./', '', $libraryDir);
+		$this->rootDir = basename($rootDir);
+		$this->cwd = getcwd();
 	}
-	
+
+	private function loadComposerJson($dir) {
+		chdir(realpath($dir));
+
+		$json = json_decode(file_get_contents($this->fileName), true);
+
+		if (isset($json['autoload']['psr-4'])) {
+			foreach ($json['autoload']['psr-4'] as $namespace => $path) {
+				chdir($path);
+
+				$this->axel->addModule(new PSR4(getcwd(), $namespace));
+			}
+		}
+
+
+		chdir($this->cwd);
+	}
+
 	public function locate($className) {
-		//This is fairly slow but it doesn't matter as all paths are cached to Class:fullpath once the file has been located once
-		//This whole class will only get loaded on requests when there is a new class to map
-		foreach (new \DirectoryIterator($this->libraryDir) as $vendors) {
-			if ($vendors->isDot()) continue;
+		//Load composer.json and use the rules defined there to reconfigure the axel instance
+		//This works recursively so only needs ot happen once.
+		if ($this->configured == false) {
+			$json = json_decode(file_get_contents($this->rootDir .  DIRECTORY_SEPARATOR . $this->fileName), true);
 
-			if ($vendors->isDir()) {
-				foreach (new \DirectoryIterator($vendors->getPathName()) as $package) {
-					//$jsonLocation = 
-					if ($package->isDot()) continue;
 
-					if ($package->isDir()) {
-						if (is_file($package->getPathName() . DIRECTORY_SEPARATOR . $this->file)) {
-							$found = $this->loadJsonFile($className, $package->getPathName());
-							if ($found) {
-								return $found;
-							}
-						}
-					}
+			$libraryDir = realpath($this->rootDir) . DIRECTORY_SEPARATOR .($json['config']['vendor-dir'] ?? 'vendor');
+
+			$this->loadComposerJson($this->rootDir);
+
+			foreach (new \DirectoryIterator($libraryDir) as $vendor) {
+				if ($vendor->isDot() || !$vendor->isDir()) continue;
+
+				foreach (new \DirectoryIterator($vendor->getPathName()) as $package) {
+					if ($package->isDot() || !$package->isDir()) continue;
+
+					if (is_file($package->getPathName() . DIRECTORY_SEPARATOR . 'composer.json')) $this->loadComposerJson($package->getPathName());
 				}
 			}
+
+			$this->configured = true;
 		}
+		return false;
 	}
 
-	private function loadJsonFile($className, $path) {
-		$json = json_decode(file_get_contents($path . DIRECTORY_SEPARATOR . $this->file));
-		if (isset($json->autoload)) {
-			foreach ($json->autoload as $type => $value) {
-				if ($type == 'psr-4') return $this->psr($className, $path, $value);
-				else if ($type == 'classmap') return $this->classmap($value);
-			}
-		}
-	}
-
-	private function psr($className, $path, $value) {
-		$path = rtrim($path, DIRECTORY_SEPARATOR);
-		foreach ($value as $rootNs => $dir) {
-			$module = new PSR4($path . DIRECTORY_SEPARATOR . $dir, $rootNs);
-			$this->axel->addModule($module);
-			return $module->locate($className);
-		}
-	}
-
-	private function classmap($value) {
-		//TODO
-	}
 }
